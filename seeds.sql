76e4650908568f467b4011c41063f2f436dea78d
Tyler Cornwell <tylercornwell@hotmail.com>
2019-07-22 11:18:14 -0700

    seeds file

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tomb Raider", "Video Games", 49.95, 150),
+  ("Wolfenstein", "Video Games", 59.99, 200),
+  ("Chicken", "Food", 24.50, 50),
+  ("Glasses", "Apparel", 75.00, 5),
+  ("Black Shirt", "Apparel", 54.25, 35),
+  ("Tent", "Camping", 42.42, 42),
+  ("Sleeping bag", "Camping", 15.00, 25),
+  ("Shrek", "Films", 25.50, 57),
+  ("Life", "Board Games", 30.50, 35),
+  ("Candy Land", "Board Games", 19.95, 23);
+
+
+
+
+
+
